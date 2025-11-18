84155616c809d295f62608ba182c7ff6e00adfa8
Emmanuel Martin <em.martin456@gmail.com>
2025-11-18 12:40:56 +0100

    package redux & react-redux installé + mise en place du store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import PhoneComponent from "./components/PhoneComponent.jsx";
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
 
 const App = () => {
 
   return (
-    <div className="section-one">
-      <PhoneComponent />
-    </div>
+    <Provider store={store}>
+      <div className="section-one">
+        <PhoneComponent />
+      </div>
+    </Provider>
   )
 }
 
---
+++ src/redux/store.jsx
@@ -0,0 +1,8 @@
+import { createStore } from "redux";
+import phoneReducer from "./phone/reducerPhone";
+
+
+const store = createStore(phoneReducer)
+
+
+export default store;
